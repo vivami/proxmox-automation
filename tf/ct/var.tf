f831e17c6773790eda1be12005ebb1717ec20e7c
Richard Garcia <103012321+richardgarciajr@users.noreply.github.com>
2022-09-25 22:41:31 -0500

    Update var.tf
    Formatting and consistency.

--- tf/ct/var.tf
+++ tf/ct/var.tf
@@ -1,45 +1,45 @@
 variable "proxmox_host" {
-	type = map
-     default = {
-       pm_api_url = "https://10.0.42.50:8006/api2/json"
-       pm_user = "root@pam"
-       target_node = "pve"
-     }
+  type = map(any)
+  default = {
+    pm_api_url  = "https://10.0.42.50:8006/api2/json"
+    pm_user     = "root@pam"
+    target_node = "pve"
+  }
 }
 
 variable "vmid" {
-	default     = 300
-	description = "Starting ID for the CTs"
+  default     = 300
+  description = "Starting ID for the CTs"
 }
 
 
 variable "hostnames" {
-  description = "Containers to be created"
   type        = list(string)
   default     = ["prod-ct"]
+  description = "Containers to be created"
 }
 
 
 variable "rootfs_size" {
-	description = "Root filesystem size in GB"
-	default = "2G"
+  default     = "2G"
+  description = "Root filesystem size in GB"
 }
 
 variable "ips" {
-    description = "IPs of the containers, respective to the hostname order"
-    type        = list(string)
-	default     = ["10.0.42.83"]
+  type        = list(string)
+  default     = ["10.0.42.83"]
+  description = "IPs of the containers, respective to the hostname order"
 }
 
 variable "user" {
-	default     = "root"
-	description = "Ansible user used to provision the container"
+  default     = "root"
+  description = "Ansible user used to provision the container"
 }
 
 variable "ssh_keys" {
-	type = map
-     default = {
-       pub = "~/.ssh/id_ed25519-pwless.pub"
-       priv = "~/.ssh/id_ed25519-pwless"
-     }
+  type = map(any)
+  default = {
+    pub  = "~/.ssh/id_ed25519-pwless.pub"
+    priv = "~/.ssh/id_ed25519-pwless"
+  }
 }
